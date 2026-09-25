e43c60b00a618101604a3ddcaa51d9fd1ecdb5a5
Liu <427783149@qq.com>
2026-09-25 23:37:33 +0800

    Initial commit

---
+++ build/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/link.d"
+  "CMakeFiles/calculator.dir/src/calculator.c.o"
+  "CMakeFiles/calculator.dir/src/calculator.c.o.d"
+  "CMakeFiles/calculator.dir/src/main.c.o"
+  "CMakeFiles/calculator.dir/src/main.c.o.d"
+  "calculator"
+  "calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/calculator.dir/src/calculator.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/calculator.dir/src/calculator.c.o: \
+ /home/ubuntu/workspace/make-cmake-recurit-LiuYijiang/cmake-task/src/calculator.c \
+ /usr/include/stdc-predef.h \
+ /home/ubuntu/workspace/make-cmake-recurit-LiuYijiang/cmake-task/include/calculator.h
